35955441d7cd0f199b039044426300eb114dbb79
Frank Wang <fraankwang@gmail.com>
2016-01-30 16:43:40 -0500

    added base code for Main, Game, and Grid classes

---
+++ src/cellsociety_team03/SegregationGrid.java
@@ -0,0 +1,21 @@
+/**
+ * Authors: Frank Wang, Jeremy Schreck, Madhav Kumar
+ */
+
+package cellsociety_team03;
+import java.util.Map;
+
+
+public class SegregationGrid extends Grid {
+
+	public SegregationGrid(Map<String, String> params) {
+		super(params);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void step(){
+		
+	}
+
+}
