741f76515d58c60146c0b68ba9defd532cd56da5
Frank Wang <fraankwang@gmail.com>
2016-01-31 16:41:43 -0500

    forgot to include GridCell class (replaced Cell class)

---
+++ src/cellsociety_team03/GridCell.java
@@ -0,0 +1,52 @@
+package cellsociety_team03;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
+
+
+public abstract class GridCell {
+    private State myCurrentState;
+    private State myNextState;
+    private Color myColor;
+    private int mySize;
+    private Shape myShape; // TODO: can we figure out a way to make it Node?
+
+    public GridCell (State currentState, int size, Shape s) {
+        myCurrentState = currentState;
+        myShape = s;
+        myColor = myCurrentState.getColor();
+        myShape.setFill(myColor);
+        mySize = size;
+    }
+
+    public State getMyCurrentState () {
+        return myCurrentState;
+    }
+
+    public void setMyCurrentState (State myCurrentState) {
+        this.myCurrentState = myCurrentState;
+    }
+
+    public State getMyNextState () {
+        return myNextState;
+    }
+
+    public void setMyNextState (State myNextState) {
+        this.myNextState = myNextState;
+    }
+    
+    public void transitionStates(){
+    	this.myCurrentState = this.myNextState;
+    	this.myNextState = null;
+    }
+
+    public Shape getMyShape () {
+        return myShape;
+    }
+
+    public void setMyColor (Color myColor) {
+        this.myColor = myColor;
+        this.getMyShape().setFill(this.myColor);
+    }
+
+}
