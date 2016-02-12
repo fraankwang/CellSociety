7e6d8672fbeaad2681e9ea9b6c98132fd4a486eb
Jeremy Schreck <jes85@duke.edu>
2016-02-12 00:35:24 -0500

    Rectangle grid work under new refactored changes that will make it easier to add triangle and hexagon grids

--- src/views/RectangleGridView.java
+++ src/views/RectangleGridView.java
@@ -6,20 +6,21 @@ import javafx.scene.Group;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Shape;
 
+
 public class RectangleGridView extends GridView {
 
     private GridPane gp;
-    
+
     public RectangleGridView (GridCell[][] cells) {
         super(cells);
         // TODO Auto-generated constructor stub
     }
-    
+
     @Override
-    protected Group createUI(){
+    protected Group createUI () {
         GridPane gridPane = new GridPane();
         gridPane.setPrefSize(MainView.GRID_VIEW_SIZE, MainView.GRID_VIEW_SIZE);
-       
+
         for (int row = 0; row < getMyRows(); row++) {
             for (int col = 0; col < getMyColumns(); col++) {
                 Shape shape = getMyCellShapes()[row][col];
@@ -29,24 +30,21 @@ public class RectangleGridView extends GridView {
 
         Group group = new Group();
         group.getChildren().add(gridPane);
-        
+
         gp = gridPane;
-        
+
         return group;
     }
 
-    public void updateCellShape(GridCell cell){
+    public void updateCellShape (GridCell cell) {
         super.updateCellShape(cell);
-        gp.getChildren().remove(overallIndexFromRC());
-        gp.add(cell.getMyShape(), cell.getMyGridLocation().getCol(), cell.getMyGridLocation().getRow());
-        
-        //TODO: might need to update the view too depending on how we implement it 
-        // (i.e. do something similar to remove/add that we did for the gridpane
-    }
-    private int overallIndexFromRC(){
-        
-        
-        return getMyRows() * getMyColumns() + getMyColumns();
+
+        Location l = cell.getMyGridLocation();
+        Shape old = getMyCellShapes()[l.getRow()][l.getCol()];
+        gp.getChildren().remove(old);
+        gp.add(cell.getMyShape(), cell.getMyGridLocation().getCol(),
+               cell.getMyGridLocation().getRow());
+
     }
 
 }
