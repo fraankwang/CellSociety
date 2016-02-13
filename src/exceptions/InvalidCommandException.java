bc9132cec4286c005bbd5ea5cfc112b7586ba150
Frank Wang <fraankwang@gmail.com>
2016-02-13 04:03:20 -0500

    added unused custom exception

---
+++ src/exceptions/InvalidCommandException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+/**
+ * Authors: Frank Wang, Jeremy Schreck, Madhav Kumar
+ */
+
+@SuppressWarnings("serial")
+public class InvalidCommandException extends RuntimeException {
+    public InvalidCommandException (String message) {
+        super(message);
+    }
+}
